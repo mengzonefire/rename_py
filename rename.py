f91f37c41d3a4b7ffdf80065cca4955188b49649
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2020-07-08 12:43:17 +0800

    Create rename.py
    first upload

---
+++ rename.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+import traceback
+import locale
+import os
+path = os.getcwd()
+locale.setlocale(locale.LC_ALL, locale='zh-CN')
+Action_Get_List = False
+Action_Rename_File = True
+file_list = []
+rename_list = []
+
+
+def get_file_list():
+    global file_list
+    file_list = os.listdir(path)
+    file_list = sorted(file_list, key=locale.strxfrm)
+    if "list.txt" in file_list:
+        file_list.remove('list.txt')
+        return Action_Rename_File
+    else:
+        return Action_Get_List
+
+
+def write_list_file(m_list):
+    with open('list.txt', 'w', encoding='utf-8') as f:
+        for file in m_list:
+            f.write(file + '\n')
+
+
+def read_list_file():
+    global rename_list
+    with open('list.txt', 'r', encoding='utf-8') as f:
+        rename_list = f.read()
+        rename_list = rename_list.split('\n')
+        rename_list.remove('')
+
+
+def write_error_file(error_log):
+    with open('error_log.txt', 'w', encoding='utf-8') as f:
+        f.write(error_log)
+
+
+def main():
+    global file_list, rename_list
+    if Action_Rename_File == get_file_list():
+        read_list_file()
+        if len(rename_list) == len(file_list):
+            for i in range(0, len(rename_list)):
+                os.rename(file_list[i], rename_list[i])
+            write_list_file(sorted(rename_list, key=locale.strxfrm))
+        else:
+            write_error_file("文件数量与目录不匹配，请删除list.txt再重新运行程序")
+    elif Action_Get_List == get_file_list():
+        write_list_file(file_list)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        traceback.print_exc()
+        write_error_file(e)
+

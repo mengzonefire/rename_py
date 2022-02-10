d8396ed5eb5aecd3779992c603b71f56e0612328
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2022-02-10 21:25:26 +0800

    update 1.0.1
    添加部分文本提示
    
    list.remove前添加判断防止报错
    
    移除log写入功能, 改为保留程序命令行窗口

--- rename.py
+++ rename.py
@@ -1,6 +1,10 @@
 # -*- coding: UTF-8 -*-
-import traceback,locale,os,re,sys
-version = 'v1.0'
+import traceback
+import locale
+import os
+import re
+import sys
+version = 'v1.0.1'
 Action_Get_List = False
 Action_Rename_File = True
 file_list = []
@@ -29,9 +33,10 @@ def str2key(mystr):
 def get_file_list():
     global file_list
     file_list = os.listdir(exe_path)
-    file_list.remove(exe_name)
+    if exe_name in file_list:
+        file_list.remove(exe_name)
     file_list = sorted(file_list, key=str2key)
-    if "list.txt" in file_list:
+    if 'list.txt' in file_list:
         file_list.remove('list.txt')
         return Action_Rename_File
     else:
@@ -49,16 +54,13 @@ def read_list_file():
     with open('list.txt', 'r', encoding='utf-8') as f:
         rename_list = f.read()
         rename_list = rename_list.split('\n')
-        rename_list.remove('')
-
-
-def write_error_file(error_log):
-    with open('error_log.txt', 'w', encoding='utf-8') as f:
-        f.write(error_log)
+        if '' in rename_list:
+            rename_list.remove('')
 
 
 def main():
     global file_list, rename_list
+    print('version: {}'.format(version))
     locale.setlocale(locale.LC_ALL, locale='zh-CN')
     if Action_Rename_File == get_file_list():
         read_list_file()
@@ -66,16 +68,18 @@ def main():
             for i in range(0, len(rename_list)):
                 os.rename(file_list[i], rename_list[i])
             write_list_file(sorted(rename_list, key=str2key))
+            print('文件重命名完成, 并自动更新list.txt目录排序')
         else:
-            write_error_file("文件数量与目录不匹配，请删除list.txt再重新运行程序")
+            print('文件数量与目录不匹配, 请删除list.txt再重新运行程序')
     elif Action_Get_List == get_file_list():
+        print('已生成目录文件list.txt, 修改目录并重新执行程序即可重命名文件')
         write_list_file(file_list)
 
 
 if __name__ == '__main__':
     try:
-        print('version: {}'.format(version))
         main()
     except Exception as e:
         traceback.print_exc()
-        write_error_file(str(e))
+    finally:
+        input('按回车键退出')
